d3f9c3b6dd28967e331255df07dd477afd49c0fd
simonkaruga <simonkaruganjoroge@gmail.com>
2025-09-08 11:44:35 +0300

    Add origin

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+  //...
+]
+function superbowlWin(record) {
+    const win = record.find(item => item.result === "W");
+    return win ? win.year : undefined;
+}
+
+// You can test the function with the following line:
+// console.log(superbowlWin(record)); // Should return "2015"
+
+module.exports = { superbowlWin };
